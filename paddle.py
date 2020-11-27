ab5c009619c6e08710825880b7bf41b88fb19739
sfave <sebpfave@gmail.com>
2020-11-27 14:04:10 -0500

    Paddle class refactoring move methods

--- paddle.py
+++ paddle.py
@@ -7,9 +7,8 @@ PADDLE_HEIGHT = 100
 PADDLE_START_POSITION = (350, 0)
 MOVE_INCREMENT = 20
 
+
 # Classes
-
-
 class Paddle(Turtle):
     """ docstring """
 
@@ -17,15 +16,14 @@ class Paddle(Turtle):
         super().__init__()
         self.penup()
         self.shape("square")
-        self.shapesize(stretch_len=5, stretch_wid=1)
-        self.setheading(90)
+        self.shapesize(stretch_len=1, stretch_wid=5)  # starting size = 20, 20
         self.color("white")
         self.goto(PADDLE_START_POSITION)
 
     def move_up(self):
         """ Move paddle up """
-        self.forward(MOVE_INCREMENT)
+        self.sety(self.ycor()+MOVE_INCREMENT)
 
     def move_down(self):
         """ Move paddle down """
-        self.backward(MOVE_INCREMENT)
+        self.sety(self.ycor()-MOVE_INCREMENT)
