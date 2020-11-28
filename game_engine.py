5f12c1275c7ade2e7af84e348ff34d7b7aa61b10
sfave <sebpfave@gmail.com>
2020-11-27 19:52:06 -0500

    Resolve paddle collision detection bug

--- game_engine.py
+++ game_engine.py
@@ -11,10 +11,10 @@ def detect_paddle_collision(ball, player1_paddle, player2_paddle):
     """ Evaluate if the ball hit the front side of the paddle """
 
     # check collision with player 1 paddle
-    if ball.distance(player1_paddle) <= limit_paddle_distance and ball.xcor() <= -limit_paddle_face:
+    if ball.distance(player1_paddle) <= limit_paddle_distance and ball.xcor() == -limit_paddle_face:
         return True
     # check collision with player 2 paddle
-    elif ball.distance(player2_paddle) <= limit_paddle_distance and ball.xcor() >= limit_paddle_face:
+    elif ball.distance(player2_paddle) <= limit_paddle_distance and ball.xcor() == limit_paddle_face:
         return True
     else:
         return False
--- game_parameters.py
+++ game_parameters.py
@@ -18,8 +18,9 @@ PLAYER2_START_POSITION = (350, 0)
 BALL_RADIUS = 10
 
 # Variables
-limit_horz = WINDOW_WIDTH/2
+limit_horz = WINDOW_WIDTH/2-OFFSET
 limit_vert = WINDOW_HEIGHT/2-OFFSET
 limit_paddle_face = PLAYER2_START_POSITION[0]-PADDLE_WIDTH/2-BALL_RADIUS
-limit_paddle_distance = PADDLE_HEIGHT/2
+limit_paddle_distance = sqrt(
+    (PADDLE_HEIGHT/2)**2 + (PADDLE_WIDTH/2+BALL_RADIUS)**2)
 limit_paddle_position = WINDOW_HEIGHT/2-PADDLE_HEIGHT/2
